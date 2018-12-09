76d21b15cc22f9d531719c270ea4d2a59b497857
jerry <llj.joy@163.com>
2018-12-09 18:48:18 +0800

    删除多余文件

---
+++ CoreFrame.Identity/Controllers/ValueController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CoreFrame.Util;
+using System.IO;
+using System.Drawing.Imaging;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CoreFrame.IdentityServer.Controllers
+{
+    [Authorize]
+    public class ValueController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public ValueController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public ActionResult Index()
+        {
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            string contentRootPath = _hostingEnvironment.ContentRootPath;
+
+            return Content(webRootPath + "\n" + contentRootPath);
+        }
+ 
+
+    }
+}
---
+++ CoreFrame.Identity/Models/Config.cs
@@ -0,0 +1,58 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreFrame.IdentityServer.Models
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("FileStoreApi","FileStoreApi")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "myblogclient666",
+                    RefreshTokenExpiration= TokenExpiration.Sliding,
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,//授权方式，这里采用的是客户端认证模式，只要ClientId，以及ClientSecrets正确即可访问对应的AllowedScopes里面的api资源
+                    ClientSecrets = { new Secret("myblogsecret999".Sha256()) },
+                    AlwaysIncludeUserClaimsInIdToken=true,
+                    AllowedScopes = new List<string> {"FileStoreApi"}
+
+                }
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        public static List<TestUser> GetTestUsers()
+        {
+            return new List<TestUser> {
+                new TestUser{
+                    SubjectId = "1",
+                    Username = "test",
+                    Password = "1234"
+                }
+            };
+        }
+    }
+}
